1bd21026e44b43eb8c0b204c80ba924509ad4d2f
Piyush Owalekar <owlekar@gmail.com>
2026-08-21 17:33:35 +0530

    Change student name from 'Yash' to 'Piyush'

--- Assignment 02/Q3.js
+++ Assignment 02/Q3.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     if (req.url === '/student') {
         const student = {
             id: 101,
-            name: "Yash",
+            name: "Piyush",
             course: "B.tech",
             semester: 3,
             city: "Mumbai"
